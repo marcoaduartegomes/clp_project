bd44a3e87d554c4f0669500642d7b20beb3f2555
BrenoLD94 <breno.ld@hotmail.com>
2018-06-06 15:55:46 -0300

    Add files via upload

--- project.ml
+++ project.ml
@@ -1,84 +1,69 @@
-(* Projeto da Matéria - Conceitos de Linguagem de Programação(CLP) *)
+(* DECLARAÇÃO DAS FUNÇÕES *)
 
-(* Declaração das Listas *)
-let listaLogico = ['!';'&';'@'];;
-let listaAritmetico = ['+';'-';'*';'/';'%';'^'];;
-let expressao = "-!-";;
-let resposta_verifica = ref '0';;
-let resposta_indice = ref 0;;
+(* verifica se o parenteses é aberto *)
+let eh_parenteses_aberto c = 
+	if c = '(' then
+		true
+	else
+		false;;
 
-(* Função que transforma string em lista tendo cada um dos caracteres *)
-let explode s =
-	let rec exp i l =
-		if i<0 then l else exp (i - 1) (s.[i] :: l) in
-	exp (String.length s -1) []
 
-(* Função que verifica se a string contem um termo de uma lista *)		
-let verifica = fun s lista resposta ->
-  let lista_string = explode s in
-  let tamanho_lista_string = List.length lista_string in
-  let tamanho_lista = List.length lista in	 
-  let char = ref '0' in
-  let existe = ref '0' in
-	try
-  for i = 0 to tamanho_lista_string - 1 do
-	  for j = 0 to tamanho_lista - 1 do
-	    char := List.nth lista_string i;
-	    existe := List.nth lista j;
-	    resposta := if char = existe
-			then 's'
-			else 'n';
-			if !resposta = 's' then raise Exit;
-	  done
-  done;
-	false
-	with Exit -> true;;
+(* verifica se o parenteses é fechado *)
+let eh_parenteses_fechado c = 
+	if c = ')' then
+		true
+	else
+		false;;
 
-(* Função que retorna o indice na lista da string do primeiro termo comum*)
-(*  entre a string e a lista*)		
-let pega_indice = fun s lista resposta ->
-  let lista_string = explode s in
-  let tamanho_lista_string = List.length lista_string in
-  let tamanho_lista = List.length lista in	 
-  let char = ref '0' in
-  let existe = ref '0' in
-	try
-  for i = 0 to tamanho_lista_string - 1 do
-	  for j = 0 to tamanho_lista - 1 do
-	    char := List.nth lista_string i;
-	    existe := List.nth lista j;
-	    resposta := if char = existe
-			then i
-			else -1;
-			if !resposta = i then raise Exit;
-	  done
-  done;
-	false
-	with Exit -> true;;
 
-(* Função que separa a string em uma lista contendo os 'tokens' *)
+(* verifica se tem parenteses *)
+let rec tem_parenteses exp pos num_p boolean =
+	if pos = String.length exp then
+		if num_p = 0 then
+			boolean = false
+		else
+			boolean = true
+	else
+		if ((eh_parenteses_aberto (String.get exp pos)) || (eh_parenteses_fechado (String.get exp pos))) then
+			boolean = tem_parenteses exp (pos+1) (num_p+1) boolean
+		else 
+			boolean = tem_parenteses exp (pos+1) num_p boolean;;
 
-let rec separa_exp = fun s lista ->
-  let resposta = ref '0' in
-	let teste = ref true in
-	  teste := verifica s listaAritmetico resposta;
-		if !resposta = 's' then
-   	  let teste2 = ref true in
-	    let resposta_indice = ref 0 in
-      teste2 := pega_indice s listaAritmetico resposta_indice;
-		  let char = ref '0' in
-		  let lista_s = explode s in
-		  let tamanho = List.length lista_s in
-		  char := List.nth lista_s !resposta_indice;
-		  lista := List.append [!char] !lista;  
-      if (tamanho == 1) then 
-			  lista else 
-			  separa_exp (String.sub s (!resposta_indice+1) (tamanho - !resposta_indice - 1)) lista;
-    else lista;;
+(* verifica se os parenteses estão na ordem correta *)				
+let rec parenteses_ordem exp pos num_p=
+	(* parenteses não pode começar com ) *)
+  if pos < String.length exp then
+		print_int(0);
+  	if String.get exp pos = ')' && pos = 0 then
+  		false
+  	else
+    	if pos = String.length exp then
+    		(* então os parenteses estão em ordem correta*)
+    		if num_p = 0 then 
+    			true
+    		else
+    			(* então os parenteses não estão na ordem correta*)
+    			false
+    	else
+    		if eh_parenteses_aberto (String.get exp pos) then
+    			parenteses_ordem exp (pos+1) (num_p+1)
+    		else
+    			if eh_parenteses_fechado (String.get exp pos) then
+    				parenteses_ordem exp (pos+1) (num_p-1)
+    			else	
+    				parenteses_ordem exp (pos+1) num_p;;
+(*
+let parenteses_join exp pos num_p boolean = 
+	if tem_parenteses exp pos num_p boolean then
+		if parenteses_ordem exp pos num_p then
+			true
+		else
+			false							*)																											
+(*=================================================================================================================================*)			
+				
+let exp = "(2+3)*4";;
 
-(* ========================================================================  *)
-(* MAIN *)
+(*tem_parenteses exp 0 0 true;;*)
 
-let lista = ref [];;
-let resposta = separa_exp expressao lista;;
-Printf.printf "%c" (List.nth !lista 1);;
+print_string(string_of_bool(parenteses_ordem exp 0 0));;
+
