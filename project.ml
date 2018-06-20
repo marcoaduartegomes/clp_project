7bf9efbd689a4bdd60059da583badc9f2cb1a0ce
cvieira1 <anjoscaio@yahoo.com>
2018-06-19 23:07:55 -0300

    VERSAO FINAL FASE 1 "TUDO JUNTO"

--- project.ml
+++ project.ml
@@ -195,4 +195,129 @@ let resposta = separa_exp expressao lista;;
 (* Printf.printf "%c" (List.nth !lista 1);; *)
 let resposta2 = verifica_rac expressao teste;;
 Printf.printf "%b" !teste;;
-				 	
+
+(*========================================INÍCIO DA DECLARAÇÃO DE FUNÇÕES===================================================================================*)
+
+(* mostra a lista *)
+let rec print_list = function
+[] -> ()
+| e::l -> print_string e ; print_string " " ; print_list l;;
+
+(*função para tirar todos espaços *)
+let tira_espace exp =
+	Str.global_replace (Str.regexp " \\|\t") "" exp;;
+
+(*========================================FIM DA DECLARAÇÃO DE FUNÇÕES===================================================================================*)
+
+		
+expressao := tira_espace !expressao;;
+let lista_char = explode !expressao;;
+
+let palavra = ref "";;
+let letra = ref ' ';;
+(* !!!!INICIO FUNCAO HACK!!!! *)
+for indice=0 to ((List.length lista_char)-1) do
+	letra := List.nth lista_char indice;
+	if e_variavel_min(!letra) || e_variavel_max(!letra) || e_digito !letra then
+		palavra := (!palavra)^(String.make 1 !letra);
+		if indice = ((List.length lista_char)-1) then
+			begin
+				if (e_variavel_min(!letra) = false) && (e_variavel_max(!letra)=false) && (e_digito !letra = false)  then begin
+					lista := List.append [!palavra] !lista;
+					palavra := ""	;
+					lista := List.append [String.make 1 !letra] !lista;end
+				else begin
+					lista := List.append [!palavra] !lista;
+					palavra := "";
+				end
+			end
+		else
+			if (e_variavel_min(!letra) = false) && (e_variavel_max(!letra)=false) && (e_digito !letra = false)  then begin
+				lista := List.append [!palavra] !lista;
+				palavra := ""	;
+				lista := List.append [String.make 1 !letra] !lista;end
+			else begin
+				if (e_variavel_max(!letra)=true) then begin
+				  lista := List.append [!palavra] !lista;
+				end	
+			end
+done;;
+
+let verifica_sintaxeFun = fun s lista ->
+	  let tamanho = String.length s in
+		let lista_s = explode s in
+		let lista_var = ref [] in
+		let indice_atual = ref 0 in 
+		let var = ref "" in
+		for i = 0 to (tamanho - 1) do
+			if (String.get s i) = '(' || (String.get s i) = ')' || (String.get s i) = ',' then
+				begin
+				lista_var := (Char.escaped(String.get s i) :: !lista_var);
+				end
+			else
+				begin
+				var := !var ^ Char.escaped(String.get s i);						
+				if(i+1) < tamanho then 
+					begin
+			    if (String.get s (i+1)) = '(' || (String.get s (i+1)) = ')' || (String.get s (i+1)) = ',' then
+					  begin
+				    lista_var := (!var :: !lista_var);
+					  var := "";
+					  end
+					end
+				end		
+		done;
+		lista := List.rev !lista_var;;
+
+let token = fun lista lista_final ->
+	let tamanho = List.length !lista in
+	let resposta_int = ref false in
+	let resposta_rac = ref false in
+	let regexp_div = Str.regexp "[:]" in
+	let rodada1 = ref true in
+	for i = 0 to (tamanho - 1) do
+		verifica_int (List.nth !lista i) resposta_int;
+		let tamanho_string = String.length (List.nth !lista i) in
+		for j = 0 to (tamanho_string) do
+		if (Str.string_match regexp_div (List.nth !lista i) j) then
+			begin
+		  verifica_rac (List.nth !lista i) resposta_rac;
+			if !resposta_rac then
+				rodada1 := false;
+			end;
+		done;	
+		rodada1 := true;
+		if (ehVariavel (List.nth !lista i) 0 0 true) then
+			  begin
+				if i = 0 then
+					begin	
+				  lista_final := ("<fun,"^(List.nth !lista i)^">") :: !lista_final;
+					end
+				else
+					begin
+				  lista_final := ("<var,"^(List.nth !lista i)^">") :: !lista_final;						
+					end
+				end;
+		if (List.nth !lista i) = "(" then	
+			  begin
+				lista_final := "<sep,abreParenteses>" :: !lista_final;
+				end;
+		if (List.nth !lista i) = ")" then	
+			  begin
+				lista_final := "<sep,fechaParenteses>" :: !lista_final;
+				end;
+		if (List.nth !lista i) = "," then	
+			  begin
+				lista_final := "<sep,virgula>" :: !lista_final;
+				end;
+		if !resposta_int then	
+			  begin
+				lista_final := ("<int,"^(List.nth !lista i)^">") :: !lista_final;
+				end;
+		if !resposta_rac then	
+			  begin
+				lista_final := ("<rac,"^(List.nth !lista i)^">") :: !lista_final;
+				end;
+		resposta_rac := false;		
+	done;
+	print_list (List.rev !lista_final);;	 
